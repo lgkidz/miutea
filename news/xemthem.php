426d87242538b676ef2aa7b82b311a152f81f1c0
unknown <lgkidz1@gmail.com>
2017-10-31 11:52:44 +0700

    xemthem

--- news/xemthem.php
+++ news/xemthem.php
@@ -41,6 +41,21 @@ body {
 	font-size: 14px;	
 }
 </style>
+
+<?php
+	include("../connection/connection.php");
+	$con = new connection();
+	$con = $con->con;
+	if(isset($_GET["id"])){
+		$id = $_GET["id"];
+	}
+	else{
+		$id = 0;
+	}
+	$statement = $con->prepare("SELECT * FROM tintuc where ma_tin=$id");
+	$statement->execute();
+	$new = $statement->fetch(PDO::FETCH_OBJ);
+?>
 <body>
 	<?php include('header.php'); ?>
 	<div style="margin-top: -20px;" id="about_us">
@@ -50,77 +65,25 @@ body {
 
 <div class="w3-row-padding w3-content" style="max-width:1400px">
   <div class="w3-twothird w3-center">
-    <img src="img/gcs_thumb_577c773c6e0b496787977b4a-2016-07-06-031304.jpg" alt="Chèn ảnh ngoài trang new" style="width:100%">
 	  <br><br><br>
-   <h1 class="w3-xxxlarge w3-text-brown"><b>Tiêu đề</b></h1>
-    <div class="w3-justify w3-left">
-		<br> <br> 
-      <p>(NỘI DUNG) Miutea là một thương hiệu trà sữa vô cùng nổi tiếng với các bạn trẻ Không chỉ có kem cheese ngon đặc sắc làm nức lòng các milktea lover, mà trà hoa quả với vị trà thanh mát từ lá trà được chắt lọc ở những đồi chè nổi tiếng kết hợp với trái cây tươi thật sự chinh phục trái tim thực khách.
-		  <br> <br>
-		  Bên cạnh đó việc kết hợp trang trí đa dạng nhiều không gian phù hợp với sở thích cũng là một trong những điểm nhấn đặc biệt của Miutea..</p>
-    </div>
+   <h1 class="w3-xxxlarge w3-text-brown"><b><?php echo $new->tieu_de; ?></b></h1>
 	  <br>
 	    <div class="w3-row-padding">
     <div class="w3-half">
-      <img src="img/khong-gian-quan-1652764-1483344609.jpg" style="width:400px; height:300px" onclick="onClick(this)" alt="<br>Phong cách hiện đại, trẻ trung">
-		
-		<p class="w3-padding-64" align="left"> Để thu hút đặc biệt đến khách hàng thì việc thiết kế không gian cùng những hình ảnh ấn tượng, độc đáo mang nét cá tính riêng biệt cũng được lựa chọn. Ở đây từ thiết kế nội thất quán trà sữa đến cách trang trí, sắp xếp rồi màu sắc luôn có sự sáng tạo, độc đáo riêng của quán. </p>
-			</div>
-    <div class="w3-half">
-      <p class="w3-padding-24" align="left"> Phong cách quán trà sữa được thiết kế hiện đại vẫn là xu hướng yêu thích của một bộ phận khách hàng trẻ. Hình ảnh, đường nét đơn giản kết hợp những màu sắc trang nhã, thanh lịch tạo nên nét cuốn hút vừa hiện đại vừa trẻ trung. Bên cạnh đó là các điểm nhấn ấn tượng, nhiều ý tưởng sáng tạo bắt kịp xu hướng thị trường sẽ tạo cảm giác thích thú cho khách hàng </p>
-		<br> <br> <br> <br> <br> <br> <br>
-      <img src="img/gcs_thumb_57349f356e0b4910cfa2aecb-2016-06-20-145217.jpg" style="width:400px; height:300px" onclick="onClick(this)" alt="<br>Trang trí bằng hình ảnh ấn tượng">
-    </div>
-<br> <br>
-			<div class="w3-row-padding w3-center">
-				<br> <br>
-		<p class="w3-padding-16">Ngoài ra còn có góc sử dụng các vật nuôi chủ yếu như chó, mèo cũng sẽ là một nguồn cảm hứng lớn nếu bạn là người yêu thú cưng. Hãy cùng xem qua 1 số hình ảnh của chúng ta nhé!
-		</p>
-			<div class="w3-row-padding w3-center">
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_57b5881b6e0b495ef1cceb02-2016-08-18-100415.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_57b588176e0b495ef7d0cbc8-2016-08-18-100411.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_57f5c2216e0b494c1a262ee9-2016-10-06-031652.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_577c773c6e0b496787977b4a-2016-07-06-031304.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-  </div>
-
-  <div class="w3-row-padding w3-center w3-section">
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_5768a93f6e0b496d85b65740-2016-06-21-024110.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_5768a9366e0b496d88b66095-2016-06-21-024058.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="" onclick="onClick(this)">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_57349f2f6e0b4910cfa2aec2-2016-06-20-145216.jpg" style="width:100%" class="w3-hover-opacity" alt=""  onclick="onClick(this)">
-    </div>
-
-    <div class="w3-col m3">
-      <img src="img/gcs_thumb_5768a93e6e0b496d8ab66fd8-2016-06-21-024110.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="">
-    </div>
-  </div>
-	
-	  </div>
-  <!-- Modal for full size images on click-->
-  <div id="modal01" class="w3-modal w3-black" style="padding-top:35" onclick="this.style.display='none'">
+      <img src="img/<?php echo $new->hinh_anh ?>" alt="[IMG]" style="width:100%" onclick="onClick(this)">
+      <div id="modal01" class="w3-modal w3-black" style="padding-top:35" onclick="this.style.display='none'">
     <span class="w3-button w3-black w3-xxlarge w3-display-bottomright">×</span>
     <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-16">
       <img id="img01" class="w3-image">
       <p id="caption"></p>
     </div>
   </div>
+			</div>
+			<div class="w3-row-padding w3-center">
+				<br> <br>
+		<p class="w3-padding-16"><?php echo $new->noi_dung;?>
+		</p>
+	  </div>
   </div>
 	</div>
   <div class="w3-third">
